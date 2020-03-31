4bba919738df48c231360d64ccdaac523a38dec6
Julia Selyutina <jusereda@gmail.com>
2020-03-31 21:32:37 +0300

    added background.

--- venv/include/drowpy.py
+++ venv/include/drowpy.py
@@ -6,6 +6,7 @@ def main():
     draw_image(window)
     window.getMouse()
 
+
 def draw_image(window):
     house_x, house_y = window.width // 2, window.height * 2 // 3
     house_width = window.width // 3
@@ -14,11 +15,21 @@ def draw_image(window):
     draw_background(window)
     draw_house(window, house_x, house_y, house_width, house_height)
 
-def draw_background(widow):
+
+def draw_background(window):
+    earth = gr.Rectangle(gr.Point(0, window.height // 2),
+                         gr.Point(window.width - 1, window.height - 1))
+    earth.setFill("green")
+    earth.draw(window)
+    scy = gr.Rectangle(gr.Point(0, 0),
+                       gr.Point(window.width - 1, window.height // 2))
+    scy.setFill("cyan")
+    scy.draw(window)
+
+
+def draw_house(window, x, y, width, height):
     pass  # TODO
 
-def draw_house(window, house_x, house_y, house_width, house_height):
-    pass  # TODO
 
 if __name__ == "__main__":
     main()
