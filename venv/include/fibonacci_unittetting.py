6dbd6d4bd04a693501e35abce3c9ba96c9a6fbec
Julia Selyutina <jusereda@gmail.com>
2020-04-03 00:55:06 +0300

    Added new file with testing fibonacci unit.

--- venv/include/fibonacci_unittetting.py
+++ venv/include/fibonacci_unittetting.py
@@ -1,5 +1,5 @@
 import unittest
-from fibonacci import fibo
+
 
 class TestFibonacciNumbers(unittest.TestCase):
     def test_zero(self):
@@ -9,6 +9,27 @@ class TestFibonacciNumbers(unittest.TestCase):
         for n, fib_n in (1, 1), (2, 1), (3, 2), (4, 3), (5, 5):
             with self.subTest(i=n):
                 self.assertEqual(fibo(n), fib_n)
+
+    def test_negative(self):
+        with self.subTest(i=1):
+            self.assertRaises(ArithmeticError, fibo, -1)
+        with self.subTest(i=1):
+            self.assertRaises(ArithmeticError, fibo, -10)
+
+    def test_fractional(self):
+        self.assertRaises(ArithmeticError, fibo, 2.5)
+
+
+def fibo(n: int) -> int:
+    """
+    Calculates fibonacci number by it's index
+    """
+    if not isinstance(n, int) or n < 0:
+        raise ArithmeticError
+    f = [0, 1] + [0] * (n - 1)
+    for i in range(2, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+    return f[n]
 
 
 if __name__ == '__main__':
