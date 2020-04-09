4abf34234a074932aeb71288d80a967ee40be0f1
Julia Selyutina <jusereda@gmail.com>
2020-04-09 23:49:41 +0300

    Added abstract-factory pattern example.

--- venv/include/abstract_factory_pattern.py
+++ venv/include/abstract_factory_pattern.py
@@ -23,7 +23,7 @@ class HeroFactory(ABC):
 # которые будут специфичны для класса персонажа
 
 class WarriorFactory(HeroFactory):
-
+    # воин
     class Hero:
         def __init__(self, name):
             self.mame = name
@@ -54,19 +54,88 @@ class WarriorFactory(HeroFactory):
             return "Power"
 
 
+class MageFactory(HeroFactory):
+    # маг
+    class Hero:
+        def __init__(self, name):
+            self.name = name
+            self.spell = None
+            self.weapon = None
+            self.armor = None
+
+        def add_spell(self, spell):
+            self.spell = spell
+
+        def add_weapon(self, weapon):
+            self.weapon = weapon
+
+        def hit(self):
+            print(f"Mage hits with {self.weapon.hit()}")
+            self.weapon.hit()
+
+        def cast(self):
+            print(f"Mage casts {self.spell.cast()}")
+            self.spell.cast()
+
+    class Weapon:
+        def hit(self):
+            return "Staff"
+
+    class Spell:
+        def cast(self):
+            return "Fireball"
 
 
+class AssassinFactory(HeroFactory):
+    # убийца
+    class Hero:
+        def __init__(self, name):
+            self.name = name
+            self.spell = None
+            self.weapon = None
+            self.armor = None
+
+        def add_weapon(self, weapon):
+            self.weapon = weapon
+
+        def add_spell(self, spell):
+            self.spell = spell
+
+        def hit(self):
+            print(f"Assassin hits with {self.weapon.hit()}")
+            self.weapon.hit()
+
+        def cast(self):
+            print(f"Assassin casts {self.spell.cast()}")
+
+    class Weapon:
+        def hit(self):
+            return "Dagger"
+
+    class Spell:
+        def cast(self):
+            return "Invisibillity"
+
+
+# Определим функцию, создающую персонажей
 def create_hero(factory):
     hero = factory.create_hero("Nagibator")
     weapon = factory.create_weapon()
     spell = factory.create_spell()
-
     hero.add_weapon(weapon)
     hero.add_spell(spell)
-
     return hero
 
 
+# Попробуем создать персонажей различных классов
 player = create_hero(WarriorFactory())
 player.hit()
 player.cast()
+print()
+player1 = create_hero(MageFactory())
+player1.hit()
+player1.cast()
+print()
+player2 = create_hero(AssassinFactory())
+player2.hit()
+player2.cast()
