7da5a7fe0ee52d454424b86f2396c20284ac5229
Julia Selyutina <jusereda@gmail.com>
2020-04-20 15:21:38 +0300

    Added class descriptor example.

--- venv/include/descriptor_example_2.py
+++ venv/include/descriptor_example_2.py
@@ -1,5 +1,16 @@
+class NoDataDescr:
+    # non-data descriptor
+    def __set_name__(self, owner, name):
+        self.__name = name
+
+    def __get__(self, instance, owner):
+        return  "NoDataDescr __get__"
+
+
 class CoordValue:
-    def __init__(self, name):
+    # data-descriptor
+    def __set_name__(self, owner, name):
+        print(name)
         self.__name = name
 
     def __get__(self, instance, owner):
@@ -10,8 +21,10 @@ class CoordValue:
 
 
 class Point:
-    coordX = CoordValue("coordX")
-    coordY = CoordValue("coordY")
+    noData = NoDataDescr()
+
+    coordX = CoordValue()
+    coordY = CoordValue()
 
     def __init__(self, x=0, y=0):
         self.coordX = x
@@ -22,3 +35,15 @@ pt1 = Point(1, 2)
 pt2 = Point(10, 20)
 print(pt1.coordX, pt1.coordY)
 print(pt2.coordX, pt2.coordY)
+
+a = Point(5, 6)
+
+# receive result of NoDataDescr._get__
+b = a.noData
+
+# new local property noData
+a.noData = "new test"
+c = a.noData
+
+print(f"value of NoDataDescr = '{b}'.")
+print(f"value of new local property noData = '{c}'")
