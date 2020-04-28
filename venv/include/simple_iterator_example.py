ea99f2297e1c6fcd0d62a98f17c78b488569cdf7
Julia Selyutina <jusereda@gmail.com>
2020-04-28 17:49:26 +0300

    Added simple iterator example.

---
+++ venv/include/simple_iterator_example.py
@@ -0,0 +1,21 @@
+class MyRangeIterator:
+    def __init__(self, top):
+        self.top = top
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.top:
+            raise StopIteration
+
+        current = self.current
+        self.current += 1
+        return current
+
+
+if __name__ == "__main__":
+    counter = MyRangeIterator(3)
+    for it_val in counter:
+        print(it_val)
